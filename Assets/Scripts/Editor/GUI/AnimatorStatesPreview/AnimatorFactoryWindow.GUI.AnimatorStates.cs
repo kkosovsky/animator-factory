483918fdbdfe8739dea532099346b71bbdbb7766
Kamil Kosowski <kosowski.ka@gmail.com>
2025-08-03 13:21:35 +0200

    Add preliminary gui implementation

--- Assets/Scripts/Editor/GUI/AnimatorStatesPreview/AnimatorFactoryWindow.GUI.AnimatorStates.cs
+++ Assets/Scripts/Editor/GUI/AnimatorStatesPreview/AnimatorFactoryWindow.GUI.AnimatorStates.cs
@@ -1,10 +1,18 @@
+using UnityEngine;
+
 namespace AnimatorFactory
 {
     public partial class AnimatorFactoryWindow
     {
         void DrawAnimatorUI(PrefabHierarchyListItem item)
         {
-            
+            if (!item.gameObject.HasComponent<Animator>())
+            {
+                return;
+            }
+
+            Animator animator = item.gameObject.GetComponent<Animator>();
+            // TODO: Get all states of the animator. Show them as a horizontal, scrollable(if needed) list of buttons.
         }
     }
 }
