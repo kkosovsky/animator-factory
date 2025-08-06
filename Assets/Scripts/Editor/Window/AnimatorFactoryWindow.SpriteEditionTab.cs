86bccca80f7c93b1a81304b4259709893706da1c
Kamil Kosowski <kosowski.ka@gmail.com>
2025-08-06 09:37:50 +0200

    Add texture selection field

--- Assets/Scripts/Editor/Window/AnimatorFactoryWindow.SpriteEditionTab.cs
+++ Assets/Scripts/Editor/Window/AnimatorFactoryWindow.SpriteEditionTab.cs
@@ -1,3 +1,5 @@
+using UnityEditor.UIElements;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace AnimatorFactory
@@ -10,8 +12,45 @@ namespace AnimatorFactory
             {
                 style = { flexGrow = 1 }
             };
+
+            CreateSpriteSelectionSection(container: content);
+            CreateSpriteImage(sprite: null, width: 0, height: 0, container: content);
             
             return content;
+        }
+        
+        void CreateSpriteSelectionSection(VisualElement container)
+        {
+            _prefabField = new ObjectField(label: Strings.textureSelectionLabel)
+            {
+                objectType = typeof(Texture2D),
+                allowSceneObjects = false
+            };
+
+            _prefabField.RegisterValueChangedCallback(callback: OnTextureSelected);
+            container.Add(child: _prefabField);
+        }
+        
+        void CreateSpriteImage(Sprite sprite, float width, float height, VisualElement container)
+        {
+            Image image = new()
+            {
+                sprite = sprite,
+                scaleMode = ScaleMode.ScaleToFit,
+                style =
+                {
+                    width = width,
+                    height = height,
+                    alignSelf = Align.Center,
+                    display = DisplayStyle.None
+                }
+            };
+            
+            container.Add(child: image);
+        }
+
+        void OnTextureSelected(ChangeEvent<Object> evt)
+        {
         }
     }
 }
--- Assets/Scripts/Editor/Window/AnimatorFactoryWindow.Strings.cs
+++ Assets/Scripts/Editor/Window/AnimatorFactoryWindow.Strings.cs
@@ -10,6 +10,7 @@ namespace AnimatorFactory
             internal const string menuItemName = "Window/Animator Factory";
             internal const string windowTitle = "Animator Factory";
             internal const string prefabSelectionLabel = "Select Prefab:";
+            internal const string textureSelectionLabel = "Select Texture:";
             internal const string hierarchyLabel = "GameObject Hierarchy:";
             internal const string animatorStatesTabLabel = "Animator States";
             internal const string spriteEditionTabLabel = "Sprite Edition";
