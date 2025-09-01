5229652a88c1e8ec38c8018ef4dafee3add04885
Kamil Kosowski <kosowski.ka@gmail.com>
2025-09-01 18:40:02 +0200

    Add prefab hierarchy to variant tab

--- Editor/Scripts/Tabs/PrefabVariantEdition/PrefabSelection/View/PrefabVariantsEditionView.cs
+++ Editor/Scripts/Tabs/PrefabVariantEdition/PrefabSelection/View/PrefabVariantsEditionView.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using AnimatorFactory.Core.UI;
+using AnimatorFactory.PrefabHierarchy;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -15,7 +16,11 @@ namespace AnimatorFactory.PrefabVariants
         FolderField _sourceFolderField;
         Label _selectedItemsLabel;
         Button _generateButton;
+        PrefabHierarchyView _hierarchyView;
+        Label _hierarchyLabel;
 
+        public PrefabHierarchyView HierarchyView => _hierarchyView;
+        
         public PrefabVariantsEditionView() => CreateUI();
 
         public void ShowSelectedItemsLabel(int count)
@@ -29,6 +34,7 @@ namespace AnimatorFactory.PrefabVariants
         {
             SetStyle();
             AddPrefabSelection();
+            AddHierarchySection();
             AddSelectedItemsLabel();
             AddGenerateButton();
         }
@@ -83,5 +89,43 @@ namespace AnimatorFactory.PrefabVariants
         }
 
         void OnGenerateButtonClicked() => GenerateButtonClicked?.Invoke();
+
+        void AddHierarchySection()
+        {
+            _hierarchyLabel = new Label(text: "Hierarchy:")
+            {
+                style =
+                {
+                    unityFontStyleAndWeight = FontStyle.Bold,
+                    marginTop = 10,
+                    marginBottom = 5,
+                    display = DisplayStyle.None
+                }
+            };
+            Add(child: _hierarchyLabel);
+
+            _hierarchyView = new PrefabHierarchyView
+            {
+                style =
+                {
+                    display = DisplayStyle.None,
+                    height = 200,
+                    marginBottom = 10
+                }
+            };
+            Add(child: _hierarchyView);
+        }
+
+        public void ShowHierarchy()
+        {
+            _hierarchyLabel.style.display = DisplayStyle.Flex;
+            _hierarchyView.style.display = DisplayStyle.Flex;
+        }
+
+        public void HideHierarchy()
+        {
+            _hierarchyLabel.style.display = DisplayStyle.None;
+            _hierarchyView.style.display = DisplayStyle.None;
+        }
     }
 }
--- Editor/Scripts/Tabs/PrefabVariantEdition/PrefabVariantsEditionTabController.cs
+++ Editor/Scripts/Tabs/PrefabVariantEdition/PrefabVariantsEditionTabController.cs
@@ -1,3 +1,5 @@
+using AnimatorFactory.PrefabHierarchy;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace AnimatorFactory.PrefabVariants
@@ -7,11 +9,13 @@ namespace AnimatorFactory.PrefabVariants
         readonly PrefabVariantsEditionView _view;
         readonly PrefabVariantsEditionViewModel _viewModel;
         readonly PrefabVariantListController _listController;
+        readonly PrefabHierarchyViewModel _hierarchyViewModel;
 
         public PrefabVariantsEditionTabController()
         {
             _view = new PrefabVariantsEditionView();
             _viewModel = new PrefabVariantsEditionViewModel();
+            _hierarchyViewModel = new PrefabHierarchyViewModel();
             _listController = new PrefabVariantListController(
                 viewModel: new PrefabVariantSelectionListViewModel(),
                 itemViewFactory: new PrefabVariantListItemFactory()
@@ -28,16 +32,34 @@ namespace AnimatorFactory.PrefabVariants
 
         void BindEvents()
         {
-            _view.PrefabSelected += _listController.OnParentPrefabSelected;
+            _view.PrefabSelected += OnPrefabSelected;
             _view.GenerateButtonClicked += _viewModel.OnGenerateClicked;
             _listController.ItemsApplied += OnVariantItemsApplied;
+            _hierarchyViewModel.HierarchyChanged += _view.HierarchyView.OnHierarchyChanged;
         }
 
         void UnbindEvents()
         {
-            _view.PrefabSelected -= _listController.OnParentPrefabSelected;
+            _view.PrefabSelected -= OnPrefabSelected;
             _listController.ItemsApplied -= OnVariantItemsApplied;
             _view.GenerateButtonClicked -= _viewModel.OnGenerateClicked;
+            _hierarchyViewModel.HierarchyChanged -= _view.HierarchyView.OnHierarchyChanged;
+        }
+
+        void OnPrefabSelected(GameObject prefab)
+        {
+            if (prefab == null)
+            {
+                _view.HideHierarchy();
+                _hierarchyViewModel.Clear();
+            }
+            else
+            {
+                _view.ShowHierarchy();
+                _hierarchyViewModel.LoadHierarchy(prefab: prefab);
+            }
+            
+            _listController.OnParentPrefabSelected(prefab: prefab);
         }
 
         void OnVariantItemsApplied(PrefabVariant[] variants)
